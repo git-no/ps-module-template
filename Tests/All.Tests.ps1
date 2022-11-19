208f8e9609401a921fba55571d200bc95cd2bb53
Dr. No <mo+ita-git@depeche.email>
2022-11-19 16:50:17 +0100

    FIX Pester test import-module missing

--- Tests/All.Tests.ps1
+++ Tests/All.Tests.ps1
@@ -1,4 +1,6 @@
 # https://pester.dev/docs/assertions/assertions#beoftype
 
+Import-Module (Join-Path $PSScriptRoot "../MODULENAME/MODULENAME.psd1") -force
+
 Invoke-Pester -Path (Get-Item ".\Tests\*.Test.ps1") -CI
 # -Exclude "TAll.Test.ps1",TemplatePowerShellModule.Tests_ps1)
